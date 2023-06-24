691ba50b3a4f67c89196d9c08867e748ee1374d3
idesheaven <marianancerutti@gmail.com>
2023-06-24 09:12:11 -0300

    Atualizações Js

--- aulas-exercicios-arrays/aula17-filter.js
+++ aulas-exercicios-arrays/aula17-filter.js
@@ -11,3 +11,23 @@ const reprovados = alunos.filter((aluno, indiceDoAluno) => {
 })
 
 console.log(reprovados);
+
+
+
+///////////////////////////// EXEMPLO DOIS:
+
+/*
+
+const idades = [1,2,3,4]
+
+const idadesMostrar = idades.filter(
+    function (idade){
+        return idade >= 3
+    }
+)
+
+console.log(idadesMostrar)
+
+// 1. o filter vai pegar o array completo | 2. o array vai ser posto no parametro idade | 3. o filter vai retornar quais sao as idades maiores que 3
+
+*/
